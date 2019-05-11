5ee6e18ec890db3d0f941d232007eebaa746edeb
pwachira900@gmail.com <pwachira900@gmail.com>
2019-05-11 05:58:45 +0300

    add more images

--- app/src/main/java/com/moringaschool/barterpata/SignupActivity.java
+++ app/src/main/java/com/moringaschool/barterpata/SignupActivity.java
@@ -67,7 +67,7 @@ public class SignupActivity extends AppCompatActivity {
         String email = _emailText.getText().toString();
         String password = _passwordText.getText().toString();
 
-        // TODO: Implement your own signup logic here.
+        //  signup logic
 
         new android.os.Handler().postDelayed(
                 new Runnable() {
