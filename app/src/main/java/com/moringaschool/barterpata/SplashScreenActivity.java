e130f6d0fd7b7cc761e9697eb1e4d569b05de1ab
pwachira900@gmail.com <pwachira900@gmail.com>
2019-05-11 04:05:06 +0300

    add more  custom font

--- app/src/main/java/com/moringaschool/barterpata/SplashScreenActivity.java
+++ app/src/main/java/com/moringaschool/barterpata/SplashScreenActivity.java
@@ -3,27 +3,41 @@ package com.moringaschool.barterpata;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        ButterKnife.bind(this);
 
         new Handler().postDelayed(
                 new Runnable() {
                     @Override
                     public void run() {
+
                         Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                         startActivity(intent);
+
+
+                        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "ostrich-regular.ttf");
+                        mAppNameTextView.setTypeface(ostrichFont);
                         finish();
+
                     }
                 },
-                1200);
+                1500);
+
 
 
 
