3e33ffe04a450af00aba5bf793835afc72ee31ca
pwachira900@gmail.com <pwachira900@gmail.com>
2019-05-11 03:50:33 +0300

    add custom font

--- app/src/main/java/com/moringaschool/barterpata/SplashScreenActivity.java
+++ app/src/main/java/com/moringaschool/barterpata/SplashScreenActivity.java
@@ -2,7 +2,10 @@ package com.moringaschool.barterpata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                },
+                1200);
+
+
+
     }
 }
