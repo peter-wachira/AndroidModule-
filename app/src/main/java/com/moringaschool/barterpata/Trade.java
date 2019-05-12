a8cd493c24c9679f5bc0a3b1e3a5507beb4f6a31
pwachira900@gmail.com <pwachira900@gmail.com>
2019-05-12 11:26:24 +0300

    add trade adapter

--- app/src/main/java/com/moringaschool/barterpata/Trade.java
+++ app/src/main/java/com/moringaschool/barterpata/Trade.java
@@ -1,14 +1,27 @@
 package com.moringaschool.barterpata;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class Trade extends AppCompatActivity {
+public class Trade extends ListActivity {
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+        // use your custom layout
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.rowlayout, R.id.label, values);
+        setListAdapter(adapter);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trade);
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/com/moringaschool/barterpata/TradeAdapter.java
@@ -0,0 +1,4 @@
+package com.moringaschool.barterpata;
+
+public class TradeAdapter {
+}
